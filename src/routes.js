b7074929f3f933aacad234a4108ee68dac8bd6b3
Erick <ericktakao28@gmail.com>
2021-09-05 18:17:19 -0300

    fix: add Forms route

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Dashboard from "./pages/Dashboard";
+import Forms from "./pages/Forms";
 
 export function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/dashboard" component={Dashboard} />
+        <Route path="/forms" component={Forms} />
       </Switch>
     </BrowserRouter>
   );
