6c329a99ad29a613b88b4ec27acebf1443fe1527
ga-sr <gabriel.10rocha@hotmail.com>
2021-09-08 17:51:14 -0300

    feat: adicionado painel de notificação
    Co-authored-by: Willian Sotocorno <Willendes@users.noreply.github.com>

--- src/pages/StudentPanel/studentPanel.routes.js
+++ src/pages/StudentPanel/studentPanel.routes.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import { Home } from "./Home";
 import { UserData } from "./UserData";
 import { SeeMore } from "./SeeMore";
+import { Notification } from "./Notification";
 
 export function StudentPanelRoutes() {
   return (
@@ -10,6 +11,7 @@ export function StudentPanelRoutes() {
       <Route exact path="/studentPanel" component={Home} />
       <Route exact path="/studentPanel/data" component={UserData} />
       <Route path="/studentPanel/students/:formsId" component={SeeMore} />
+      <Route path="/studentPanel/notification" component={Notification} />
     </Switch>
   );
 }
