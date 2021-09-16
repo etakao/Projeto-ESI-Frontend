d6605692be3b4cacdd01be8f8e29ccbae4f168a8
ga-sr <gabriel.10rocha@hotmail.com>
2021-09-16 19:10:33 -0300

    no more seeMore

--- src/pages/StudentPanel/studentPanel.routes.js
+++ src/pages/StudentPanel/studentPanel.routes.js
@@ -2,7 +2,6 @@ import { Route, Switch } from "react-router-dom";
 
 import { Home } from "./Home";
 import { UserData } from "./UserData";
-import { SeeMore } from "./SeeMore";
 import { Notification } from "./Notification";
 import { Feedback } from "./Feedback";
 
@@ -11,7 +10,6 @@ export function StudentPanelRoutes() {
     <Switch>
       <Route exact path="/studentPanel" component={Home} />
       <Route exact path="/studentPanel/data" component={UserData} />
-      <Route path="/studentPanel/students/:formsId" component={SeeMore} />
       <Route path="/studentPanel/notification" component={Notification} />
       <Route path="/studentPanel/feedback" component={Feedback} />
     </Switch>
