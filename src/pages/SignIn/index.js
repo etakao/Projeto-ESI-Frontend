65555e07ef3fb368774fa312809ce45594d94268
quilantera <gugaquilante@gmail.com>
2021-09-21 10:10:56 -0300

    autenticação do usuario

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,31 +1,29 @@
 import React, { useState } from 'react';
+import { Redirect } from "react-router-dom";
+import api from "../../services/api";
+import { login, isAuthenticated} from "../../services/auth";
 
-import { users } from './dbUser.js';
 
-import { useUser } from '../../contexts/User.js'
 import './styles.scss';
 
 export  function SignIn(){
 
-    const  {user, setUser} = useUser();
-    const [email,setEmail]= useState('');
-    const [password,setPassword]= useState('');
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
 
-    const handleSubmit = (event) =>{
+    const handleSubmit = async event =>{
         event.preventDefault();
-        if(getUser()){
-                setUser(getUser());
-                alert(user);
-        }
-        else{
-            alert("usuario nao encontrado")
-        }
-    } 
-
-    function getUser() {
-        return (users.find(user => user.email === email && user.password === password));
-    }
+          try {
+            const response = await api.post("/sessions", { email, password });
+            login(response.data.token);
+            this.props.history.push("/dashboard");
+          } catch (err) {
+            alert("Houve um problema com o login, verifique suas credenciais. T.T");
+          }
+      }
     return(
+        <>
+        {isAuthenticated() ? <Redirect to="/dashboard" /> : null}
         <section className="login">
             <div className="title-login">
               <h1> PPgSI</h1>
@@ -35,16 +33,17 @@ export  function SignIn(){
             <form className="form-login" onSubmit={handleSubmit}> 
             <div className="section-form">
                 <label>E-mail </label>
-                <input type="email" name='email' placeholder="exemplo@exemplo.com.br" onChange={e => setEmail(e.target.value)} required={true}></input>
+                <input type="email" name='email' placeholder="exemplo@exemplo.com.br" onChange={e => setEmail( e.target.value)} required={true}></input>
             </div>
             <div className="section-form">
                 <label>Senha </label>
                 <input type="password" name='password'  onChange={e => setPassword(e.target.value)} required={true}></input>
             </div>
             <div className="section-form">
-                <button type="submit " >Entrar</button>
+                <button type="submit">Entrar</button>
             </div>
             </form>
         </section>
+        </>
     )
 }
