66da0a380be17aefcfaa3956aed6b7c38a54ee50
Erick <ericktakao28@gmail.com>
2021-09-23 20:31:29 -0300

    feat: atualizando cadastro

--- src/pages/Dashboard/SignUp/index.js
+++ src/pages/Dashboard/SignUp/index.js
@@ -1,54 +1,166 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { message } from 'antd';
+
+import { useTeachers } from '../../../contexts/Teachers';
+import { useCcps } from '../../../contexts/Ccps';
+import { ccpsApi } from '../../../services/ccps';
+import { teachersApi } from '../../../services/teachers';
+import { studentsApi } from '../../../services/students';
 
 import './styles.scss';
 
 export function SignUp() {
-  const [isSigningUpStudent, setIsSigningUpStudent] = useState(true);
+  const [activeSignup, setActiveSignup] = useState(0);
 
   const [name, setName] = useState('');
-  const [ra, setRa] = useState('');
+  const [usp_number, setUspNumber] = useState('');
+  const [teacher_id, setAdvisor] = useState('');
+  const [ccp_id, setCcp] = useState('');
+  const [lattes, setLattes] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const { teachers } = useTeachers();
+  const { ccps } = useCcps();
+
   const signUpForm = [
     {
+      type: 'input',
       labelText: "Nome",
       labelFor: "name",
       inputId: "name",
       inputType: "text",
       inputOnChange: e => setName(e.target.value),
-      isStudentOnly: false
+      visibleTo: [0, 1, 2]
     },
     {
+      type: 'input',
       labelText: "Número USP",
-      labelFor: "ra",
-      inputId: "ra",
+      labelFor: "uspNumber",
+      inputId: "uspNumber",
       inputType: "text",
-      inputOnChange: e => setRa(e.target.value),
-      isStudentOnly: true
+      inputOnChange: e => setUspNumber(e.target.value),
+      visibleTo: [2]
     },
     {
-      labelText: "Emal",
+      type: 'input',
+      labelText: "Lattes",
+      labelFor: "lattes",
+      inputId: "lattes",
+      inputType: "text",
+      inputOnChange: e => setLattes(e.target.value),
+      visibleTo: [2]
+    },
+    {
+      type: 'select',
+      labelText: "Orientador",
+      labelFor: "advisor",
+      selectId: "advisor",
+      selectData: teachers,
+      selectValue: teacher_id,
+      selectOnChange: e => setAdvisor(e.target.value),
+      visibleTo: [2]
+    },
+    {
+      type: 'select',
+      labelText: "CCP",
+      labelFor: "ccp",
+      selectId: "ccp",
+      selectData: ccps,
+      selectValue: ccp_id,
+      selectOnChange: e => setCcp(e.target.value),
+      visibleTo: [1]
+    },
+    {
+      type: 'input',
+      labelText: "Email",
       labelFor: "email",
       inputId: "email",
       inputType: "email",
       inputOnChange: e => setEmail(e.target.value),
-      isStudentOnly: false
+      visibleTo: [0, 1, 2]
     },
     {
+      type: 'input',
       labelText: "Senha",
       labelFor: "password",
       inputId: "password",
       inputType: "password",
       inputOnChange: e => setPassword(e.target.value),
-      isStudentOnly: false
+      visibleTo: [0, 1, 2]
     },
   ];
 
-  function handleSubmit(e) {
+  async function handleSubmit(e) {
     e.preventDefault();
 
-    console.log(name, email, password, ra);
+    message.loading({
+      key: "signingup",
+      content: "Cadastrando...",
+      duration: 9999
+    });
+
+    switch (activeSignup) {
+      case 0:
+        try {
+          const response = await ccpsApi.create({
+            name,
+            email,
+            password
+          });
+          console.log(response);
+          if (response.status === 200) {
+            message.destroy("signingup");
+            message.success("CCP cadastrada com sucesso!");
+          }
+        } catch (error) {
+          console.log(error);
+          message.destroy("signingup");
+          message.error("Erro ao cadastrar CCP, tente novamente...");
+        }
+        break;
+      case 1:
+        try {
+          const response = await teachersApi.create({
+            name,
+            email,
+            password,
+            ccp_id
+          });
+          if (response.status === 200) {
+            message.destroy("signingup");
+            message.success("Orientador cadastrado com sucesso!");
+          }
+        } catch (error) {
+          console.log(error);
+          message.destroy("signingup");
+          message.error("Erro ao cadastrar orientador, tente novamente...");
+        }
+        break;
+      case 2:
+        try {
+          const response = await studentsApi.create({
+            name,
+            email,
+            password,
+            teacher_id,
+            usp_number,
+            lattes
+          });
+          if (response.status === 200) {
+            message.destroy("signingup");
+            message.success("Aluno cadastrado com sucesso!");
+          }
+        } catch (error) {
+          console.log(error);
+          message.destroy("signingup");
+          message.error("Erro ao cadastrar aluno, tente novamente...");
+        }
+        break;
+      default:
+        break;
+    }
   }
 
   return (
@@ -56,41 +168,49 @@ export function SignUp() {
       <h2>Cadastro de usuário</h2>
       <div className="signup-tabs">
         <span
-          className={isSigningUpStudent ? "active-tab" : "inactive-tab"}
-          onClick={() => setIsSigningUpStudent(true)}
+          className={activeSignup === 0 ? "active-tab" : "inactive-tab"}
+          onClick={() => setActiveSignup(0)}
         >
-          Aluno
+          CCP
         </span>
         <span
-          className={isSigningUpStudent ? "inactive-tab" : "active-tab"}
-          onClick={() => setIsSigningUpStudent(false)}
+          className={activeSignup === 1 ? "active-tab" : "inactive-tab"}
+          onClick={() => setActiveSignup(1)}
         >
           Orientador
+        </span>
+        <span
+          className={activeSignup === 2 ? "active-tab" : "inactive-tab"}
+          onClick={() => setActiveSignup(2)}
+        >
+          Aluno
         </span>
       </div>
 
       <form onSubmit={handleSubmit}>
-        {isSigningUpStudent ? (
-          signUpForm.map(input => {
+        {signUpForm.map(input => {
+          if ((input.visibleTo).includes(activeSignup)) {
             return (
-              <>
-                <label htmlFor={input.labelFor}>{input.labelText}</label>
-                <input id={input.inputId} type={input.inputType} onChange={input.inputOnChange} />
-              </>
-            )
-          })
-        ) : (
-          signUpForm.map(input => {
-            if (!input.isStudentOnly) {
-              return (
+              input.type === 'input' ? (
                 <>
                   <label htmlFor={input.labelFor}>{input.labelText}</label>
                   <input id={input.inputId} type={input.inputType} onChange={input.inputOnChange} />
                 </>
+              ) : (
+                <>
+                  <label htmlFor={input.labelFor}>{input.labelText}</label>
+                  <select id={input.selectId} value={input.selectValue} onChange={input.selectOnChange}>
+                    <option value="" disabled hidden>Escolha um {input.labelText}</option>
+                    {input.selectData.map(selectOpt => (
+                      <option value={selectOpt.id}>{selectOpt.name}</option>
+                    ))}
+                  </select>
+                </>
               )
-            }
-          })
-        )}
+
+            )
+          }
+        })}
         <button type="submit">Cadastrar</button>
       </form>
     </div >
