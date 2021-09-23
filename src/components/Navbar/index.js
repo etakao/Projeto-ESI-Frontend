bfb19d145646532c535805665e824ca97074cbc6
quilantera <gugaquilante@gmail.com>
2021-09-23 17:26:21 -0300

    autenticacao usuario

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,16 +1,24 @@
 import React, { useEffect, useRef } from 'react';
-
+import { useHistory, Link } from 'react-router-dom';
 import { FiLogOut, FiUser } from 'react-icons/fi';
+
 
 import { useUser } from '../../contexts/User';
 
+import { logout} from "../../services/auth";
 import './styles.scss';
 
 export function Navbar({ setNavbarHeight }) {
   const navbarHeight = useRef(null);
-
-  const { user } = useUser();
-
+  const history = useHistory();
+  const { user, removeUser } = useUser();
+  
+const  Logout = (event)=> {
+  event.preventDefault();
+  removeUser()
+  logout()
+  history.push("/");
+}
   useEffect(() => {
     setNavbarHeight(navbarHeight.current.clientHeight);
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -21,7 +29,9 @@ export function Navbar({ setNavbarHeight }) {
       <h2>PPgSI | Painel do {user.user_type}</h2>
       <div className="navbar-menu">
         <h3><FiUser /> {user.name}</h3>
-        <FiLogOut className="log-out" title="Sair" />
+      
+        <FiLogOut className="log-out" title="Sair" onClick={Logout}/>
+     
       </div>
     </div>
   );
--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,31 +1,70 @@
 import React, { useState } from 'react';
+import { Redirect, useHistory} from "react-router-dom";
 
-import { users } from './dbUser.js';
+import { message } from 'antd';
 
-import { useUser } from '../../contexts/User.js'
+import api from "../../services/api";
+import { login, isAuthenticated} from "../../services/auth";
+import { useUser } from '../../contexts/User';
+
 import './styles.scss';
 
 export  function SignIn(){
+   
+    const history = useHistory();
+    const {user, setUser}= useUser();
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
 
-    const  {user, setUser} = useUser();
-    const [email,setEmail]= useState('');
-    const [password,setPassword]= useState('');
-
-    const handleSubmit = (event) =>{
+    const handleSubmit = async event =>{
         event.preventDefault();
-        if(getUser()){
-                setUser(getUser());
-                alert(user);
-        }
-        else{
-            alert("usuario nao encontrado")
-        }
-    } 
+        
+        message.loading({
+            key: "logging",
+            content: "Logando...",
+            duration: 9999
+          });
 
-    function getUser() {
-        return (users.find(user => user.email === email && user.password === password));
-    }
+          try {
+            const response = await api.post("/login", { email, password });
+                login(response.data.token)
+                const users = response.data;
+                for(const index in users){
+                    if (index==='student'){
+                        users[index]["user_type"]='Aluno';
+                        users[index]["level"]= 11;
+                        setUser(users[index]);
+                    }
+                    if (index==='teacher'){
+                        users[index]["user_type"]='Orientador';
+                        users[index]["level"]= 7;
+                        setUser(users[index]);
+                    }
+                    if (index==='ccp'){
+                        users[index]["user_type"]='CCP';
+                        users[index]["level"]= 5;
+                        setUser(users[index]);
+                    }
+                    if (index==='admin'){
+                        users[index]["user_type"]='Admin';
+                        users[index]["level"]= 3;
+                        setUser(users[index]);  
+                    }
+                }
+                message.destroy("logging");
+                console.log(user);
+                history.push("/dashboard");
+                message.success("Bem-vindo!");
+               
+          } catch (err) {
+            console.log(err);
+            message.destroy("logging");
+            message.error("Erro ao efetuar login, tente novamete...");
+          }
+      }
     return(
+        <>
+        {isAuthenticated() ? <Redirect to="/dashboard" /> : null}
         <section className="login">
             <div className="title-login">
               <h1> PPgSI</h1>
@@ -35,16 +74,17 @@ export  function SignIn(){
             <form className="form-login" onSubmit={handleSubmit}> 
             <div className="section-form">
                 <label>E-mail </label>
-                <input type="email" name='email' placeholder="exemplo@exemplo.com.br" onChange={e => setEmail(e.target.value)} required={true}></input>
+                <input type="email" name='email' placeholder="exemplo@exemplo.com.br" onChange={e => setEmail( e.target.value)} required={true}></input>
             </div>
             <div className="section-form">
                 <label>Senha </label>
                 <input type="password" name='password'  onChange={e => setPassword(e.target.value)} required={true}></input>
             </div>
             <div className="section-form">
-                <button type="submit " >Entrar</button>
+                <button type="submit">Entrar</button>
             </div>
             </form>
         </section>
+        </>
     )
 }
