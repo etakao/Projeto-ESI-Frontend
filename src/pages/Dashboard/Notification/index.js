786c3d63b74cd0ba5b18b5136e24837c51a00408
ga-sr <gabriel.10rocha@hotmail.com>
2021-09-23 22:31:46 -0300

    finaly
    Co-authored-by: Willian Sotocorno <Willendes@users.noreply.github.com>

--- src/pages/Dashboard/Notification/index.js
+++ src/pages/Dashboard/Notification/index.js
@@ -1,58 +1,53 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
-import { useState } from 'react';
 import { useUser } from '../../../contexts/User';
-import { students } from '../../../db';
 
-import { FiInbox } from 'react-icons/fi';
+import { useHistory } from "react-router-dom"
 
 import './styles.scss';
 
 export function Notification() {
-  const [student, setStudent] = useState({});
 
   const { user } = useUser();
 
-  function getStudentInfo(numero_usp) {
-    setStudent(students.find(student => student.numero_usp === numero_usp));
-  }
+  const history = useHistory();
 
-  useEffect(() => {
-    const numero_usp = parseInt(user.numero_usp);
-    getStudentInfo(numero_usp);
-  }, [user.numero_usp]);
+  function routeChange(route, who){
+    history.push({
+      pathname: `/dashboard/feedback/${route}`,
+      state: {
+        from: who
+    }})
+  }
 
   return (
     <div className="container">
       <div className="title">
-        <h2>Bem vindo, {student.name}</h2>
+        <h2>Bem vindo, {user.name}</h2>
       </div>
       <div className="subTitle">
         <h3>Últimas atualizações</h3>
-        <div className="seeAll" onClick={() => { alert("ver todas") }}>
-          <h3><FiInbox />Ver todas</h3>
-        </div>
       </div>
-      <div className="notification" onClick={() => { alert("ver notificação") }}>
+      <div className="notification" onClick={() => { routeChange('2', 'CCP')}}>
         <div>
           <h3>Parecer final</h3>
           <p>CCP disponibilizou sua avaliação final</p>
         </div>
-        <span>23/08/2021</span>
+        <span>01/07/2021</span>
       </div>
-      <div className="notification" onClick={() => { alert("ver notificação") }}>
+      <div className="notification" onClick={() => { routeChange('2', 'Orientador') }}>
+        <div>
+          <h3>Parecer final</h3>
+          <p>Orientador disponibilizou sua avaliação final</p>
+        </div>
+        <span>23/06/2021</span>
+      </div>
+      <div className="notification" onClick={() => { routeChange('3', 'CCP') }}>
         <div>
           <h3>Parecer final</h3>
           <p>CCP disponibilizou sua avaliação final</p>
         </div>
-        <span>23/08/2021</span>
-      </div>
-      <div className="notification" onClick={() => { alert("ver notificação") }}>
-        <div>
-          <h3>Parecer final</h3>
-          <p>CCP disponibilizou sua avaliação final</p>
-        </div>
-        <span>23/08/2021</span>
+        <span>20/12/2020</span>
       </div>
     </div>
   );
